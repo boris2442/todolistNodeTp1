a2ade26b9d71d8ce5677591a6c237f7e7aeafe4d
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-07 19:06:51 +0100

    debug

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,26 +1,103 @@
-// const express = require('express');
-const Todo=require('../models/Todo')
-const getIndex=(req, res)=>{
+// // const express = require('express');
+// const Todo = require("../models/Todo");
+// const getIndex = (req, res) => {
+//   res.render("index");
+// };
 
-        res.render('index');
+// const postIndex = (req, res) => {
+//   // console.log(req.body.title);
+//   Todo.find({}, (err, todos) => {
+//     if (err) console.log(err);
+//     res.render("index", {
+//       todos: todos,
+//     });
+//   });
+//   const newTodo = new Todo({
+//     title: req.body.title,
+//     completed: false,
+//   });
+//   newTodo.save().then(() => console.log("todo added"));
 
-}
+//   res.redirect("/");
+// };
 
-const postIndex=(req, res)=>{
-    
-        // console.log(req.body.title);
-        const newTodo=new Todo(({
-            title:req.body.title,
-            completed:false
-        }))
-        res.redirect('/');
-        // res.send("formulaire soumis avec success")
-
-}
+// module.exports = {
+//   getIndex: getIndex,
+//   postIndex: postIndex,
+// };
 
 
-module.exports={
-    getIndex:getIndex,
-    postIndex:postIndex,
- 
-}
+
+
+
+
+
+
+
+
+// const Todo = require("../models/Todo");
+
+// const getIndex = (req, res) => {
+//   Todo.find({}, (err, todos) => {
+//     if (err) console.log(err);
+//     res.render("index", {
+//       todos: todos,
+//     });
+//   });
+// };
+
+// const postIndex = (req, res) => {
+//   const newTodo = new Todo({
+//     title: req.body.title,
+//     completed: false,
+//   });
+
+//   newTodo.save()
+//     .then(() => {
+//       console.log("todo added");
+//       res.redirect("/");
+//     })
+//     .catch(err => console.log(err));
+// };
+
+// module.exports = {
+//   getIndex: getIndex,
+//   postIndex: postIndex,
+// };
+
+
+
+const Todo = require("../models/Todo");
+
+const getIndex = async (req, res) => {
+  try {
+    const todos = await Todo.find({});
+    res.render("index", {
+      todos: todos,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Erreur du serveur");
+  }
+};
+
+const postIndex = async (req, res) => {
+  const newTodo = new Todo({
+    title: req.body.title,
+    completed: false,
+  });
+
+  try {
+    await newTodo.save();
+    console.log("todo added");
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Erreur du serveur");
+  }
+};
+
+module.exports = {
+  getIndex: getIndex,
+  postIndex: postIndex,
+};
