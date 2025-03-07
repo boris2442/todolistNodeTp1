f5785abcb82d9a8cb154abf96fb17efde3cb9361
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-07 15:24:59 +0100

    text

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 
-const port=3004;
+const port = 3004;
 
-
-
-
+const router = require('./routes/index');
 
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
+app.use('/', router);
 
-app.listen(port, ()=>{
-console.log(`server demarré sur le port ${port}`);
-})
+app.listen(port, () => {
+    console.log(`server démarré sur le port ${port}`);
+});
