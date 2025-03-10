34af3bea1acda0f8b667caa05dbfdbb8e444434d
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-10 10:31:37 +0100

    debugage

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const bodyParser = require('body-parser');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
 
 
-// const port = 3004;
-const PORT = process.env.PORT || 3000;
+const port = 3004;
+// const port = process.env.PORT || 3000;
 
 const router = require('./routes/index');
 
