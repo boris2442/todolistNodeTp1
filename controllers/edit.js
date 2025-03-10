c31881c79f80c6a070c283a768a2cf63d1f0f118
boris2442 <aubinborissimotsebo@gmail.com>
2025-03-10 07:25:11 +0100

    codage delete

--- controllers/edit.js
+++ controllers/edit.js
@@ -2,40 +2,58 @@
 //     res.render('edit', {title: 'Edit Post', post: req.post});
 // }
 
-
 // module.exports={
 //     getEdit: getEdit
 // }
 
-
 const Todo = require("../models/Todo");
 
-const getEdit = async (req, res) => {
-  try {
-    const todo = await Todo.findById(req.params.id);
+// const getEdit = async (req, res) => {
+//   try {
+//     const todo = await Todo.findById(req.params.id);
+//     res.render("edit", {
+//       todo: todo,
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send("Erreur du serveur");
+//   }
+// };
+
+// const postEdit = async (req, res) => {
+//   try {
+//     await Todo.findByIdAndUpdate(req.params.id, {
+//       title: req.body.title,
+//       completed: req.body.completed === 'on',
+//     });
+//     res.redirect("/");
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send("Erreur du serveur");
+//   }
+// };
+
+const getEdit = (req, res) => {
+  const { id } = req.params;
+  Todo.findById((id, todo) => {
+    if (err) console.log(todo);
     res.render("edit", {
       todo: todo,
     });
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Erreur du serveur");
-  }
+  });
 };
 
-const postEdit = async (req, res) => {
-  try {
-    await Todo.findByIdAndUpdate(req.params.id, {
-      title: req.body.title,
-      completed: req.body.completed === 'on',
-    });
+const putEdit = (req, res) => {
+  const { id } = req.params;
+  const{title} = req.body
+  Todo.findByIdAndUpdate(id, { title: title }, (err) => {
+    if (err) console.log(err);
     res.redirect("/");
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Erreur du serveur");
-  }
+  });
 };
 
 module.exports = {
   getEdit,
-  postEdit,
+
+  putEdit: putEdit,
 };
